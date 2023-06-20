e05b4a06a9ca748eb118948b1250d3d9261d2b87
David <owolagbadavid@gmail.com>
2023-06-20 02:02:48 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include "main.h"
 /* more headers goes there */
 /**
- * main - Entry point
- *
- * Return: 0
- */
+* main - Entry point
+*
+* Return: 0
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
 
+	int i = 0;
 
-	printf("_putchar\n");
-
+	while ("_putchar"[i] != '\0')
+	{
+		_putchar("_putchar"[i]);
+		i++;
+	}
+	_putchar('\n');
 
 	return (0);
 }
