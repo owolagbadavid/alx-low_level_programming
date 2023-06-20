9124e71f07c0da46bc36e41c459298ee2fc3b774
David <owolagbadavid@gmail.com>
2023-06-20 07:38:46 -0700

    ad 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/* more headers goes there */
+/**
+* main - Entry point
+*
+* Return: 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			_print_int(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
+
+/**
+* _print_int - prints an integer using putchar
+*
+* @n: the integer to print
+*
+* Return: void
+*/
+void _print_int(int n)
+{
+	int d; /* digit variable */
+
+	if (n < 0) /* if negative number */
+	{
+		_putchar('-'); /* print a minus sign */
+		n = -n; /* make n positive */
+	}
+	d = n % 10; /* get the last digit */
+	if (n / 10 > 0) /* if there are more digits */
+	{
+		_print_int(n / 10); /* print the remaining digits recursively */
+	}
+	_putchar(d + '0'); /* print the last digit */
+}
