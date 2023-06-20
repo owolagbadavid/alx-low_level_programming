0dea1a173ae6b807ac7f260acfdee8d9c601bc65
David <owolagbadavid@gmail.com>
2023-06-20 08:39:16 -0700

    fix 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /* more headers goes there */
 /**
 * main - Entry point
@@ -8,51 +8,21 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	unsigned long int i = 0, j = 1, l = 0, temp;
+	double i = 1, j = 1, temp;
+	int l;
+
 	for (l = 0; l < 50; l++)
 	{
-		_print_int(j);
+		if ( l != 49)
+			printf("%.0f, ", j);
+		else
+			printf("%.0f\n", j);
 		temp = i;
 		i = j;
 		j = temp + i;
-
-		if (l == 49)
-			_putchar('\n');
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
 	}
 
-
-	_putchar('\n');
 
 
 	return (0);
-}
-
-
-/**
-* _print_int - prints an integer using putchar
-*
-* @n: the integer to print
-*
-* Return: void
-*/
-void _print_int(int n)
-{
-	int d; /* digit variable */
-
-	if (n < 0) /* if negative number */
-	{
-		_putchar('-'); /* print a minus sign */
-		n = -n; /* make n positive */
-	}
-	d = n % 10; /* get the last digit */
-	if (n / 10 > 0) /* if there are more digits */
-	{
-		_print_int(n / 10); /* print the remaining digits recursively */
-	}
-	_putchar(d + '0'); /* print the last digit */
 }
