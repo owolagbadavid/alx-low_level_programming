582e4e68b8abd67fd09c68ca244568f5745c0754
David <owolagbadavid@gmail.com>
2023-06-20 04:20:58 -0700

    fix 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
 * _abs - prints without sign
 *
-* print_sign - prints sign
+*
 *
 * @n: The integer to check
 *
@@ -12,22 +12,28 @@
 int _abs(int n)
 {
 	int result = n * print_sign(n);
+
 	return (result);
 
 }
-
+/**
+* print_sign - prints sign
+*
+* @n: the number
+* Return: sign
+*/
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                return (1);
-        }
-        else if (n < 0)
-        {
-                return (-1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
 }
