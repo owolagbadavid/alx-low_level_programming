f72e1a9fe956d2536707c478fda2cec9475377af
David <owolagbadavid@gmail.com>
2023-06-22 02:54:05 -0700

    extras

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	unsigned long int number = 612852475143, prime = 2, max = 1;
+
+	while (prime <= number) {
+		if (number % prime == 0)
+		{
+			max = prime;
+			number = number / prime;
+		}
+		else
+			prime++;
+	}
+	printf("%lu\n", max);
+	return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_number - prints an integer using putchar
+*
+* @n: the integer to print
+*
+* Return: void
+*/
+void print_number(int n)
+{
+	int d; /* digit variable */
+
+	if (n < 0) /* if negative number */
+	{
+		_putchar('-'); /* print a minus sign */
+		n = -n; /* make n positive */
+	}
+	d = n % 10; /* get the last digit */
+	if (n / 10 > 0) /* if there are more digits */
+	{
+		print_number(n / 10); /* print the remaining digits recursively */
+	}
+	_putchar(d + '0'); /* print the last digit */
+}
