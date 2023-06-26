67fb1b72f8025f209f54eac1b63b89740b6d925a
David <owolagbadavid@gmail.com>
2023-06-26 01:09:55 -0700

    add 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _puts - stuff
+*
+* @str: pointer
+* Return: int
+*/
+void _puts(char *str)
+{
+
+
+	int counter = 0;
+
+	while (s[counter] != '\0')
+	{
+		_putchar(s[counter]);
+		counter++;
+	}
+}
+
