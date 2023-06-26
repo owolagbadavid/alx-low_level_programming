72fbb6d0c675ff30de88cf1a222f05c487a31d36
David <owolagbadavid@gmail.com>
2023-06-26 01:36:11 -0700

    add 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* print_rev - stuff
+*
+* @s: pointer
+* Return: void
+*/
+void print_rev(char *s)
+{
+
+
+	int counter = _strlen(s);
+
+	while (counter > 0)
+	{
+		_putchar(str[counter]);
+		counter--;
+	}
+	_putchar('\n');
+}
+/**
+* _strlen - stuff
+*
+* @s: pointer
+* Return: int
+*/
+int _strlen(char *s)
+{
+
+
+	int counter = 0;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+
+	return (counter);
+}
