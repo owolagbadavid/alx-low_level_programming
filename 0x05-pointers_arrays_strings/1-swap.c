a04fb2e91b9a5e2b0bc9675f565a24781378f63d
David <owolagbadavid@gmail.com>
2023-06-26 00:54:20 -0700

    add 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* swap_int - stuff
+*
+* @a: pointer
+* @b: pointer
+* Return: void
+*/
+void swap_int(int *a, int *b)
+{
+
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
+
