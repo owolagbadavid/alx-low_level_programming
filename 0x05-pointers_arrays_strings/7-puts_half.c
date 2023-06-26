0969d70fe4d2bc428996775a9836fe2cafa9792d
David <owolagbadavid@gmail.com>
2023-06-26 06:31:18 -0700

    add 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* puts_half - stuff
+*
+* @str: pointer
+* Return: int
+*/
+void puts_half(char *str)
+{
+
+
+	int counter = _strlen(str) / 2;
+
+	while (str[counter] != '\0')
+	{
+		_putchar(str[counter]);
+		counter++;
+	}
+	_putchar('\n');
+}
+
+/**
+* _strlen - stuff
+*
+* @s: pointer
+* Return: int
+*/
+int _strlen(char *s)
+{
+
+
+	int counter = 0;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+
+	return (counter);
+}
