f624ac26780c1475e16c2e6b68d1504beedfeae3
David <owolagbadavid@gmail.com>
2023-07-10 02:44:18 -0700

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+* str_concat - dups a string
+*
+* @s1: str
+* @s2: str
+* Return: *char
+*/
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	unsigned int i, size1, size = 0;
+
+	if (s1)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+		}
+		size += i;
+		size1 = i;
+	}
+	if (s2)
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+		}
+		size += i;
+	}
+	p = (char *) malloc(size + 1);
+	if (!p)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+		p[i + size1] = s2[i];
+	p[i + size1] = '\0';
+	return (p);
+
+}
