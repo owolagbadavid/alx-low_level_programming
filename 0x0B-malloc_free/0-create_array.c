da88d08ccc54a4e2c2e1d20d5574b961f30a66ec
David <owolagbadavid@gmail.com>
2023-07-10 02:10:02 -0700

    start b

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+* create_array - prints an integer using putchar
+*
+* @size: size
+* @c: char
+* Return: *char
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+
+	if (size < 1)
+	{
+		return (NULL);
+	}
+
+	p = (char*) malloc(size);
+	p[0] = c;
+	return (p);
+
+}
