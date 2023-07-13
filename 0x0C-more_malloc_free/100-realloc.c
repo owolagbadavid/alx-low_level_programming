dbf373e4de5d35eb586314706be5d9976b7f8206
David <owolagbadavid@gmail.com>
2023-07-13 02:35:36 -0700

    extras

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - function
+ * @ptr: pointer
+ * @old_size: int
+ * @new_size: int
+ *
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *np;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		np = malloc(new_size);
+		if (np == NULL)
+		{
+			free(np);
+			return (NULL);
+		}
+		return (np);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	np = malloc(new_size);
+	if (np == NULL)
+	{
+		free(np);
+		return (NULL);
+	}
+
+	for (i = 0; i < old_size; i++)
+		np[i] = *((char *)ptr + i);
+
+	free((void *) ptr);
+	return (np);
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,107 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void print_err(void);
+int is_num(char *);
+int _strlen(char *);
+/**
+ * main - entry point
+ * @argc: number of args
+ * @argv: args
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_num(s1) || !is_num(s2))
+		print_err();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
+}
+
+/**
+ * is_num - stuff
+ * @s: string
+ *
+ * Return: int
+ */
+int is_num(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - stuff
+ * @s: string
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * print_err - handles errors for main
+ *
+ * Return: void
+ */
+void print_err(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+
