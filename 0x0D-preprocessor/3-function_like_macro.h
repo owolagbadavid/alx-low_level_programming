cf424707739f82fde31767f2ea8749f0d7f1ac5d
David <owolagbadavid@gmail.com>
2023-07-17 03:10:45 -0700

    preproc

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef __FUNCTIONLIKEMACROH__
+#define __FUNCTIONLIKEMACROH__
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef __4SUMH__
+#define __4SUMH__
+#define SUM(x, y) ((x) + (y))
+#endif
