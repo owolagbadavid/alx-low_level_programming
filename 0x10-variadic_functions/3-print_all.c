2af5a37c48b5ee57cad0c8444149a3e13aecdd4b
David <owolagbadavid@gmail.com>
2023-07-20 05:23:27 -0700

    fix last error

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,37 +8,93 @@
  */
 void print_all(const char * const format, ...)
 {
-	int j = 0;
-	char *str, *seprt;
 	va_list args;
+	int i = 0, j = 0;
+	char *separator = "";
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
 
 	va_start(args, format);
-	while (format[j] && format)
-	{
-		seprt = "";
-		if (format[j + 1])
-			seprt = ", ";
-		switch (format[j])
-		{
 
-		case 'c':
-			printf("%c%s", va_arg(args, int), seprt);
-			break;
-		case 'i':
-			printf("%d%s", va_arg(args, int), seprt);
-			break;
-		case 'f':
-			printf("%f%s", va_arg(args, double), seprt);
-			break;
-		case 's':
-			str = va_arg(args, char *);
-			if (!str || !*str)
-				str = "(nil)";
-			printf("%s%s", str, seprt);
-			break;
+	while (format && (format[i]))
+	{
+		j = 0;
+
+		while (j < 4 && (format[i]  != *(funcs[j].symbol)))
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
 		}
-		j++;
+
+		i++;
 	}
+
 	printf("\n");
+
 	va_end(args);
 }
+
+
+/**
+ * print_char - Prints a char
+ * @arg: arg
+ */
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	printf("%c", letter);
+}
+
+/**
+ * print_int - Prints an int
+ * @arg: arg
+ */
+void print_int(va_list arg)
+{
+	int num;
+
+	num = va_arg(arg, int);
+	printf("%d", num);
+}
+
+/**
+ * print_float - Prints a float
+ * @arg: arg
+ */
+void print_float(va_list arg)
+{
+	float num;
+
+	num = va_arg(arg, double);
+	printf("%f", num);
+}
+
+/**
+ * print_string - Prints a string
+ * @arg: arg
+ */
+void print_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", str);
+}
+
