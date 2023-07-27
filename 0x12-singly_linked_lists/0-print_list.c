76dcae7d9492081023788f793214236a1c9154d6
David <owolagbadavid@gmail.com>
2023-07-27 04:09:07 -0700

    start

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - stuff
+ * @h: pointer to node
+ *
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
