fa2b6115d0f9bc57278706ae7b8266a43ef2e23e
David <owolagbadavid@gmail.com>
2023-07-27 04:38:04 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list
+ * @head: list to free
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
