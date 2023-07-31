8d445a1191d2144d3c80b1e686e0294a36761868
David <owolagbadavid@gmail.com>
2023-07-31 10:03:23 -0700

    0 -1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - fun
+ * listint_len - func
  * @h: pointer
  *
- * Return: int +
+ * Return: len
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
