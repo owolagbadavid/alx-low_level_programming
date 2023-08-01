56d39536316a6e9327eaeaecf54f91c5b73ea5c3
David <owolagbadavid@gmail.com>
2023-08-01 08:55:27 -0700

    extras fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,68 @@
 #include "lists.h"
+void free_listp(listp_t **);
+/**
+ * free_listp - func
+ * @head: pointer double
+ *
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
 
 /**
- * print_listint_safe - func
- * @head: header
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
  *
- * Return: size
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	long int diff;
+	listp_t *headptr, *new, *add;
 
-	while (head)
+	headptr = NULL;
+	while (head != NULL)
 	{
-		diff = head - head->next;
-		count++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+		new = malloc(sizeof(listp_t));
+
+		if (new == NULL)
 			exit(98);
+
+		new->p = (void *)head;
+		new->next = headptr;
+		headptr = new;
+
+		add = headptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&headptr);
+				return (count);
+			}
 		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		count++;
 	}
 
+	free_listp(&headptr);
 	return (count);
 }
