f12834bcd345452b1101010d8f5aa34b3d3ac7d8
David <owolagbadavid@gmail.com>
2023-08-07 04:16:42 -0700

    start

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - func
+ * @filename: name of file
+ * @letters: number of letters
+ *
+ * Return: int
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = 0, int reader = 0, writer = 0;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	reader = read(fd, buffer, letters);
+	if (reader == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	buffer[i] = '\0';
+	close(fd);
+	writer = write(STDOUT_FILENO, buffer, reader);
+	if (writer == -1 || writer != reader)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (writer);
+}
