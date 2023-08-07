8c1310a8d69f43c00fa5d61dd5291fcd2b850e8f
David <owolagbadavid@gmail.com>
2023-08-07 06:16:26 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[i])
+		while (text_content[j])
 			j++;
 		i = write(fd, text_content, j);
 		if (i != j)
