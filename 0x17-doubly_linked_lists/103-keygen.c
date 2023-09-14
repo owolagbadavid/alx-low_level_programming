fa1911a840b540af6482d19cc2396482d1ebf9e6
DAVID <owolagbadavid@gmail.com>
2023-09-14 21:47:47 +0100

    extra

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	unsigned int i, b;
 	size_t len, add;
-	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
 
 	if (argc != 2)
