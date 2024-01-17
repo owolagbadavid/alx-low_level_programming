60ba9bb7a50140895cb57fad9652ab66c630658b
DAVID <owolagbadavid@gmail.com>
2024-01-17 18:47:45 +0100

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -48,5 +48,6 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
 
 #endif
